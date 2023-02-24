fb1d7b45ae754a578aa242858d08b5070ef90c5a
hrishiw <32129358+hrishiw@users.noreply.github.com>
2023-02-24 18:17:40 +0530

    added some code

---
+++ HelloWorld/main.go
@@ -0,0 +1,15 @@
+//how do we run the code in our project..??
+//
+
+// what does package main mean..??
+package main
+
+// what does import "fmt" mean..??
+import "fmt"
+
+// what does this func thing mean
+func main() {
+	fmt.Println("Hi There!")
+}
+
+// How is main.go file organized..??
